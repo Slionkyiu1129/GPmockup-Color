b9b70c798f05193ed092d0343dbe02b571a85b62
Slionkyiu1129 <xinyuu9215@gmail.com>
2025-03-10 23:27:40 +0800

    新增InventoryManager註解
    釐清InventoryManager的意義及內容

--- Assets/Script/ItemSystem/InventoryManager.cs
+++ Assets/Script/ItemSystem/InventoryManager.cs
@@ -2,14 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//InventoryManager 是所有物品相關腳本的核心
+//管理物品清單
+//當物品變更時通知UI
+
 public class InventoryManager : MonoBehaviour
 {
     //為了讓所有腳本都有Accessbility，一個接口的概念
+    //Singleton 是一種設計模式，確保在遊戲中只有一個 InventoryManager 存在
+
     #region Singleton
     public static InventoryManager Instance;
+
     private void Awake()
     {
-        if(Instance != null && Instance != this)
+        //確保只有一個 InventoryManager 存在
+        if (Instance != null && Instance != this)
         {
             Destroy(this);
         }
@@ -17,7 +25,10 @@ public class InventoryManager : MonoBehaviour
         {
             Instance = this;
         }
+
+        //確保 InventoryManager 不會再切換場景時被刪除，物品可以跨場景使用，不會被重置
         DontDestroyOnLoad(this);
+
     }
     #endregion
 
